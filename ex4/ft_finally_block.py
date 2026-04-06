151632d126dd61e3f98cbf1c800e0ebb3300ad0d
noname <davidgarciasolis@gmail.com>
2026-04-06 19:22:34 +0200

    final del ejercicio

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -16,32 +16,31 @@ def water_plant(plant_name: str) -> None:
 
 
 def test_watering_system() -> None:
-	print("=== Garden Watering System ===")
-	print()
-	print("Testing valid plants...")
-	print("Opening watering system")
-	try:
-		valid_plants = ["Tomato", "Lettuce", "Carrots"]
-		for plant in valid_plants:
-			water_plant(plant)
-	finally:
-		print("Closing watering system")
-		print()
-	print("Testing invalid plants...")
-	print("Opening watering system")
-	try:
-		invalid_plants = ["Tomato", "lettuce", "Carrots"]
-		for plant in invalid_plants:
-			try:
-				water_plant(plant)
-			except PlantError as e:
-				print(f"Caught PlantError: {e}")
-				print(".. ending tests and returning to main")
-				return  # stop test on first error
-	finally:
-		print("Closing watering system")
-		print()
-		print("Cleanup always happens, even with errors!")
+    print("=== Garden Watering System ===")
+    print("Testing valid plants...")
+    print("Opening watering system")
+    try:
+        valid_plants = ["Tomato", "Lettuce", "Carrots"]
+        for plant in valid_plants:
+            water_plant(plant)
+    finally:
+        print("Closing watering system")
+        print()
+    print("Testing invalid plants...")
+    print("Opening watering system")
+    try:
+        invalid_plants = ["Tomato", "lettuce", "Carrots"]
+        for plant in invalid_plants:
+            try:
+                water_plant(plant)
+            except PlantError as e:
+                print(f"Caught PlantError: {e}")
+                print(".. ending tests and returning to main")
+                return
+    finally:
+        print("Closing watering system")
+        print()
+        print("Cleanup always happens, even with errors!")
 
 
 def main() -> None:
